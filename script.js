b24758aa735defc71d26ffc5090ce884a21dcec8
Yanik Perez <ypmsgf@gmail.com>
2022-10-07 10:24:51 +0200

    Erledigt Button fertig implementiert

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ function GetTask() {
     console.log("date")
 
 
+    
 
 document.addEventListener("DOMContentLoaded", () =>{
-    let goog = {
-        "id": 1,
-        "task": "adsfsdf"
-    }
+    //let goog = {
+    //    "id": 1,
+    //    "task": "adsfsdf"
+    //}
 
     
 
@@ -19,13 +20,6 @@ document.addEventListener("DOMContentLoaded", () =>{
         GetTask(); 
     
 
-    const unshow = document.querySelector('#b')
-    unshow.addEventListener("click", () => { 
-        var listItems = document.getElementsByClassName('li')
-        const tableBody = document.querySelector('#todolist');
-        tableBody.removeChild(listItems)
-        document.write('fweijo')
-    })
 
     const addTask = document.querySelector('#add')
     addTask.addEventListener("click", () => {
@@ -35,12 +29,110 @@ document.addEventListener("DOMContentLoaded", () =>{
     completed: false});
     })
     
-    console.log(goog.tasks)
+    
 
     
     
 })
 
+function taskText(){
+    const taskText = document.createElement('textarea')
+}
+
+function erledigtButton(completed, id, title){
+    const erledigtButton = document.createElement("button");
+    erledigtButton.innerText = "Erledigt!✅"
+    erledigtButton.className= id;
+    erledigtButton.onclick = function () {
+        fertig(completed, id, title)
+    }
+    return erledigtButton
+}
+
+function editButton(completed, id, title){
+    const erledigtButton = document.createElement("button");
+    erledigtButton.innerText = "edit"
+    erledigtButton.className= id;
+    erledigtButton.onclick = function () {
+        inhalt = (document.getElementById("task-" + id + "a").value)
+        alert(inhalt)
+        edit(completed, id, title, inhalt)
+        
+    }
+    
+    return erledigtButton
+}
+
+function edit(completed, id, title, inhalt) {
+    if (inhalt = null) {
+    } else {
+       
+
+        const data = {
+    "id": parseInt(id),
+    "title": inhalt,
+    "completed": completed
+    }
+        fetch("http://localhost:3000/tasks", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            
+        })
+        location.reload();
+    }
+
+    
+}
+
+function fertig(completed, id, title) {
+    if (completed) {
+        console.log("making it true")
+        const data = {
+    "id": parseInt(id),
+    "title": title,
+    "completed": false
+    }
+        fetch("http://localhost:3000/tasks", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then((response) => response.json())
+        .then((data) => {
+        })
+        location.reload();
+    } else {
+       
+
+        const data = {
+    "id": parseInt(id),
+    "title": title,
+    "completed": true
+    }
+        fetch("http://localhost:3000/tasks", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            
+        })
+        location.reload();
+    }
+
+    
+}
 
 function createCell(text) {
     const cell = document.createElement('td')
@@ -53,8 +145,13 @@ function renderTasks(tasks) {
     console.log(tasks)
     const tableBody = document.querySelector('#todolist');
     tasks.forEach((tasks) => {
+
         const tableRow = document.createElement('tr');
-        tableRow.innerText = tasks.title +" "+ tasks.completed
+        tableRow.append(createCell(tasks.id), createCell(tasks.title), createCell(tasks.completed))
+        
+        tableRow.innerText = tasks.title + ' done:' + tasks.completed
+        tableRow.className = tasks.id
+
 
         const tablerow = document.createElement('button')
         tablerow.innerText = 'delete'
@@ -62,17 +159,24 @@ function renderTasks(tasks) {
         tablerow.onclick = function() {
             del(tasks.id)
         }
-        
 
         
-        
+        const editText = document.createElement('textarea')
+        editText.id = 'task-' + tasks.id + 'a'
 
-        const tablebuton = document.createElement('button')
-        tablebuton.innerText = 'edit'
+
+        
 
         tablerow.classList.add(tasks.id);
         tableBody.appendChild(tableRow);
+        tableRow.appendChild(erledigtButton(tasks.completed, tasks.id, tasks.title))
+        if(tasks.completed=true){
+            erledigtButton.innerHTML = 'fertig'
+        }
+        tableRow.innerHTML + tasks.completed
         tableRow.appendChild(tablerow);
+        tableRow.appendChild(editButton(tasks.completed, tasks.id, tasks.title));
+        tableRow.appendChild(editText);
         
     });
 }
@@ -114,6 +218,7 @@ function del(id) {
         'Content-Type': 'application/json'
         
 })
-location.reload();
+const loschen = document.getElementsByClassName(id)
+loschen[0].remove();
 }
 
